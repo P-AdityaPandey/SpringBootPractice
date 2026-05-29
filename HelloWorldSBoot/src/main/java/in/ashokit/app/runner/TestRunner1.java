8b40c102a9e54644841607d96f9b6ce24eae2266
P-AdityaPandey98 <adityapandeymfsd@gmail.com>
2026-05-29 10:52:49 +0530

    Updated

--- HelloWorldSBoot/src/main/java/in/ashokit/app/runner/TestRunner1.java
+++ HelloWorldSBoot/src/main/java/in/ashokit/app/runner/TestRunner1.java
@@ -1,22 +1,14 @@
 package in.ashokit.app.runner;
 
-import in.ashokit.app.bean.HelloWorldBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MyRunner implements ApplicationRunner {
-
-    @Autowired
-    HelloWorldBean helloWorld;
-
+public class TestRunner1 implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments arguments){
+        System.out.println(" In TestRunner1...!!! ");
 
-        System.out.println(helloWorld.sayHello("John"));
     }
-
-
 }
--- HelloWorldSBoot/src/main/java/in/ashokit/app/runner/TestRunner2.java
+++ HelloWorldSBoot/src/main/java/in/ashokit/app/runner/TestRunner2.java
@@ -1,22 +1,14 @@
 package in.ashokit.app.runner;
 
-import in.ashokit.app.bean.HelloWorldBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class MyRunner implements ApplicationRunner {
-
-    @Autowired
-    HelloWorldBean helloWorld;
-
+public class TestRunner2 implements ApplicationRunner{
     @Override
     public void run(ApplicationArguments arguments){
-
-        System.out.println(helloWorld.sayHello("John"));
+        System.out.println("In TestRunner2....!!!");
     }
-
-
 }
+
